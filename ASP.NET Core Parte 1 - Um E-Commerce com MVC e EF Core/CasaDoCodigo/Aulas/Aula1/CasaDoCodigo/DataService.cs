105fe7c175b1876a8de70b87acbda16e56fe87a8
Douglas Cata Preta <douglaspreta@landix.com.br>
2020-03-24 11:56:36 -0300

    Inclusão do projeto desenvolvido durante o curso "ASP.NET Core parte 1: Um e-Commerce com MVC e EF Core".

---
+++ ASP.NET Core Parte 1 - Um E-Commerce com MVC e EF Core/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly AplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(AplicationContext contexto, IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            this.contexto.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+            this.produtoRepository.SaveProdutos(livros);
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+}
---
+++ ASP.NET Core Parte 1 - Um E-Commerce com MVC e EF Core/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepository<T> where T : BaseModel
+    {
+        protected readonly AplicationContext contexto;
+        protected readonly DbSet<T> dbSet;
+
+        protected BaseRepository(AplicationContext contexto)
+        {
+            this.contexto = contexto;
+            dbSet = contexto.Set<T>();
+        }
+    }
+}
