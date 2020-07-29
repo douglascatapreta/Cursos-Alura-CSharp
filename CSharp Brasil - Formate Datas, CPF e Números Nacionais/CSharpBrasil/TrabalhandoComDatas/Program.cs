775f8f1c4680df16f02666c3cf2d1f751d8f24b8
Douglas Cata Preta <douglascatapreta@gmail.com>
2020-07-29 10:28:31 -0300

    Inclusão do projeto desenvolvido durante o curso "C# Brasil: Formate datas, cpf e números nacionais".

---
+++ CSharp Brasil - Formate Datas, CPF e Números Nacionais/CSharpBrasil/TrabalhandoComDatas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComDatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2017, 09, 23);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("d"));
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-br")));
+            //Debug.WriteLine(data.ToString("d", new CultureInfo("en-us")));
+            Debug.WriteLine(data.ToString("dd/MM"));
+            Debug.WriteLine(data.ToString("dd/MM/yy"));
+
+            data = new DateTime(2017, 09, 23, 13, 14, 15, 987);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("HH:mm"));
+            Debug.WriteLine(data.ToString("HH:mm:ss.fff"));
+
+            Debug.WriteLine(data.ToString("D"));
+            Debug.WriteLine(data.ToString("m"));
+            Debug.WriteLine(data.ToString("Y"));
+            
+            Debug.WriteLine(data.ToString("G"));
+            Debug.WriteLine(data.ToString("g"));
+
+            Debug.WriteLine(data.ToString("O"));
+            Debug.WriteLine(DateTime.Parse(data.ToString("O")).ToString(""));
+
+            Debug.WriteLine(data.ToString("t"));
+            Debug.WriteLine(data.ToString("T"));
+        }
+    }
+}
